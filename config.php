5cc34f1e6b271eea001f44edaee67bb5f80831c2
Connect256 <ruqia.h.farah@gmail.com>
2025-05-31 03:45:22 +0000

    make password secure

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+DEFINE ('DB_HOST', 'glzpm.h.filess.io');
+DEFINE ('DB_USER', 'cosc4806_usenearlow');
+DEFINE ('DB_PASS'), $_ENV['DB_PASS']);
+DEFINE ('DB_DATABASE', 'cosc4806_usenearlow');
+DEFINE (DB_PORT, '3305');
+?>
